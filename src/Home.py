979db507fd4505e266278a66150511023d1ae5fa
Jeff Preston <jdpreston7@gmail.com>
2026-01-09 11:24:26 -0500

    feat: add TTS for script

--- src/Home.py
+++ src/Home.py
@@ -1,7 +1,13 @@
+import wave
+
+from piper import PiperVoice
+import simpleaudio as sa
 import streamlit as st
 
 
 def main():
+
+    st.set_page_config(layout = 'wide')
     st.title(body="Script Reader", anchor=False, text_alignment="center")
 
     cols = st.columns(2)
@@ -14,6 +20,14 @@ def main():
         st.header(body="Roles", anchor=False, text_alignment="center")
 
     go_btn = st.button(label="Go", key="go", type="primary")
+
+    if go_btn:
+        voice = PiperVoice.load('voices/en_US-amy-medium.onnx')
+
+        with wave.open('wav_files/script.wav', 'wb') as wav_file:
+            voice.synthesize_wav(script, wav_file)
+
+        st.audio('wav_files/script.wav', autoplay = True)
 
 
 if __name__ == "__main__":
